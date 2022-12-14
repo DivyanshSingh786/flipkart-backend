7cf7d3a81450780000d04356c8b2fa47e81a1ea6
Divyansh Singh <93871896+DivyanshSingh786@users.noreply.github.com>
2022-12-14 15:33:42 +0530

    bcryptjs change

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -1,6 +1,6 @@
 const User = require('../../models/user.js');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const shortid = require('shortid');
 
 
