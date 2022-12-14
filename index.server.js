ae4a0ee3da6526adce732e3e7c5f09b7bbcb61d6
Divyansh Singh <93871896+DivyanshSingh786@users.noreply.github.com>
2022-12-14 14:10:44 +0530

    Add files via upload

---
+++ index.server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+// routes
+const authRoutes = require('./src/routes/auth.js');
+const adminRoutes = require('./src/routes/admin/auth.js');
+const initialDataRoutes = require('./src/routes/admin/initialData.js');
+const categoryRoutes = require('./src/routes/category');
+const productRoutes = require('./src/routes/product');
+const cartRoutes = require('./src/routes/cart');
+const pageRoutes = require('./src/routes/admin/page');
+const addressRoutes = require('./src/routes/address');
+const orderRoutes = require('./src/routes/order');
+const adminOrderRoute = require('./src/routes/admin/order.routes');
+
+// environment variables or you can say constants
+env.config();
+
+// mongodb connection
+// mongodb+srv://ecommerceuser:xJ!2Mg5LfMJ2u#!@cluster-ecommerce-webap.dkigi0i.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster-ecommerce-webap.dkigi0i.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+).then(() => {
+  console.log('Database connected');
+});
+
+//  It parses incoming JSON requests and puts the parsed data in req.body.
+// 1.  express.json() is a built in middleware function in Express starting from v4.16.0.
+// app.use(express.json());
+// 2. bodyParser()
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'uploads')));
+app.use('/api', authRoutes);
+app.use('/api', adminRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
+app.use('/api', initialDataRoutes);
+app.use('/api', pageRoutes);
+app.use('/api', addressRoutes);
+app.use('/api', orderRoutes);
+app.use('/api', adminOrderRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+})
