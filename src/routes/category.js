0d5baf4a97eff78f3b207b3d61c98499fd2131eb
Divyansh Singh <93871896+DivyanshSingh786@users.noreply.github.com>
2022-12-14 12:33:32 +0530

    Uploaded all files

---
+++ src/routes/category.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { requireSignin, adminMiddleware } = require('../common-middleware');
+const { addCategory, getCategory, updateCategories, deleteCategories } = require('../controller/category');
+const router = express.Router();
+const multer = require('multer');
+const shortid = require('shortid');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, path.join(path.dirname(__dirname), 'uploads'))
+  },
+  fillename: function(req, file, cb){
+    cb(null, shortid.generate() + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage });
+
+router.post('/category/create', requireSignin, adminMiddleware, upload.single('categoryImage'), addCategory);
+router.get('/category/getcategory', getCategory);
+router.post('/category/update', upload.array('categoryImage'), updateCategories);
+router.post('/category/delete', deleteCategories);
+
+module.exports = router;
